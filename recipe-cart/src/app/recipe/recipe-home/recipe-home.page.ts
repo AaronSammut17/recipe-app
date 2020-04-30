030ad317be6cda2820bc31197ad9e5870ce94215
AaronSammut17 <32704546+AaronSammut17@users.noreply.github.com>
2020-04-30 12:26:38 +0200

    update
    we added the categories list and recipes to show on the home page from the dat.xml

--- recipe-cart/src/app/recipe/recipe-home/recipe-home.page.ts
+++ recipe-cart/src/app/recipe/recipe-home/recipe-home.page.ts
@@ -9,27 +9,46 @@ import { RecipeService } from 'src/app/services/recipe.service';
 export class RecipeHomePage implements OnInit {
     
   /**
-     * The category slider options.
-     */
-    public categorySlideOpts = {
-      slidesPerView: 2.25
+  * The categories available to this interface.
+  */
+  public categories: any[] = [];
+
+  /**
+  * The recipes available to this interface.
+  */
+  public recipes: any[] = [];
+
+  /**
+  * The category slider options.
+  */
+  public categorySlideOpts = {
+    slidesPerView: 2.25
   };
     
   /**
-     * The recipe slider options.
-     */
-    public recipeSlideOpts = {
-      slidesPerView: 1.25
+  * The recipe slider options.
+  */
+  public recipeSlideOpts = {
+    slidesPerView: 1.25
   };
 
   constructor(
     public recipeService: RecipeService
     ) { }
 
-  ngOnInit() {
-    console.log("Starting to load...");
-        this.recipeService.preload();
+  //use async whenever there is dynamic data is loaded into the page/app.
+  async ngOnInit()
+    {
+        console.log("Starting to load...");
+
+        // we'll wait for the function to complete before the app is shown.
+        await this.recipeService.preload();
+
+        this.categories = this.recipeService.getCategories();
+        this.recipes = this.recipeService.getRecipes(true, 5);
+
+        
         console.log("Finished.");
-  }
+    }
 
 }
