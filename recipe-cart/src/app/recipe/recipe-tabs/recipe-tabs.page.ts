e82a35f7b2343c6a370b53aa2ab5f1bbd41c7825
AaronSammut17 <32704546+AaronSammut17@users.noreply.github.com>
2020-05-06 12:00:03 +0200

    made changes
    gonna continue tomorrow

--- recipe-cart/src/app/recipe/recipe-tabs/recipe-tabs.page.ts
+++ recipe-cart/src/app/recipe/recipe-tabs/recipe-tabs.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-tabs',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeTabsPage implements OnInit {
 
-  constructor() { }
+  constructor( public route: ActivatedRoute ) { }
 
   ngOnInit() {
+    console.log(this.route);
   }
 
 }
